00e92d2b75e48ca93f09be0e8112acbc849a1ef9
Andre Suzanovich <andre.suzanovich@doverfs.com>
2021-10-20 18:13:29 +0200

    add integration test

--- test/transaction-commission.e2e-spec.ts
+++ test/transaction-commission.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('TransactionCommissionController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,17 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/transaction-commission (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/transaction-commission')
+      .send({
+        date: "2021-01-01",
+        amount: "100.00",
+        currency: "EUR",
+        client_id: 42
+      })
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .expect('{"amount":0.05,"currency":"EUR"}');
   });
 });
